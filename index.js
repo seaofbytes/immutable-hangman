2c95bb0bac2477b67524b1b2fc7e5fbf47652f68
seaofbytes <sandilukez1990@gmail.com>
2020-02-04 21:24:05 +0100

    Added callback, removed guess counter function

--- index.js
+++ index.js
@@ -2,18 +2,7 @@
 const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-// number of wrong guesses
-function wrongGuessCount(word, guesses) {
-  let counter = 0;
-  guesses.map(letter => {
-    if (word.includes(letter)) {
-      counter++;
-    }
-  });
-  return counter;
-}
-
-// display guess
+//display guess
 function showGuess(word, guesses) {
   return word
     .split('')
@@ -31,23 +20,20 @@ function isWinner(word, guesses) {
   return wordToArray.length === filteredLetters.length ? true : false;
 }
 
-// next function
 function next(word, guesses) {
   Object.freeze(guesses);
-
-  if (isWinner(word, guesses)) {
-    console.log('you win');
-  } else if (wrongGuessCount(word, guesses) >= 6) {
-    console.log('you lose');
-  } else {
-    rl.question('next letter? ', answer => {
-      console.log('player wrote:', answer);
-      const nextGuesses = [...guesses, answer];
-
+  rl.question('next letter? ', answer => {
+    const nextGuesses = [...guesses, answer];
+    console.log('Player chose:', answer);
+    if (isWinner(word, guesses)) {
+      console.log('YOU WIN !!!');
+    } else if (nextGuesses.length === 6) {
+      console.log('"YOU LOSE!!!');
+    } else {
       next(word, nextGuesses);
       console.log('show guess: ', showGuess(word, nextGuesses));
-    });
-  }
+    }
+  });
 }
 
 next('hello', []);
